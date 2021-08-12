523139be5aec8a47f93581278090474e2f4312fc
MunibH <munibh15@gmail.com>
2021-08-12 15:31:56 -0400

    some gui stuff

--- probeSelect.m
+++ probeSelect.m
@@ -8,7 +8,9 @@ Nunits = numel(h.obj.clu{probe});
 
 str = cell(Nunits, 1);
 for i = 1:Nunits
-    str{i} = ['Unit ' num2str(i) ': ' h.obj.clu{probe}(i).quality];
+    nspikes = numel(h.obj.clu{probe}(i).tm);
+    str{i} = ['Unit ' num2str(i) ': ' h.obj.clu{probe}(i).quality ...
+              '  #spks: ' num2str(nspikes)];
 end
 set(h.unitList, 'Value', 1, 'String', str);
 
--- unitSelect.m
+++ unitSelect.m
@@ -1,9 +1,3 @@
 function unitSelect(~, ~, fig)
 
-% if isfield(get(figure(542)),'Visible')
-%     my_selectivity([],[],fig);
-% end
-% 
-% figure(532);
-
 updateAxes([], [], fig);
